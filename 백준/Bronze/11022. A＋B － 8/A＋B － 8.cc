2a00ce8396914de74672b4bc219583901772fc4c
bum19 <77481223+bum19@users.noreply.github.com>
2022-12-07 20:39:01 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(void){
+    int A,B,T,x=1;
+    scanf("%d",&T);
+    while(T--){
+        scanf("%d %d",&A,&B);
+        printf("Case #%d: %d + %d = %d\n",x++,A,B,A+B);
+    }
+    return 0;
+}
