cfb8d1635d4daacb02ea75abc90685eb262212a1
bum19 <77481223+bum19@users.noreply.github.com>
2023-08-13 15:25:27 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 110 ms, Memory: 20,168 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -1,43 +1,53 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+public class Solution {
+	public static int n, m, idx, k;
+	public static List<Integer> cyphers;
 
-public class Solution{
-	static int n, m, idx, k;
-	static List<Integer> cipherList = new ArrayList<Integer>();
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
-		for(int test_case = 1; test_case <= 10; test_case++) {
-			n = Integer.parseInt(br.readLine());
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < n ; i++){
-				cipherList.add(Integer.parseInt(st.nextToken()));
-			}
-			m = Integer.parseInt(br.readLine());
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < m; i++) {
-				st.nextToken();
-				idx = Integer.parseInt(st.nextToken());
-				k = Integer.parseInt(st.nextToken());
-				for(int j = idx; j < idx + k; j++) {
-					cipherList.add(j,Integer.parseInt(st.nextToken()));
-				}
-				
-			}
-		
-			sb.append("#").append(test_case).append(" ");
-			for(int i = 0; i < 10; i++)
-				sb.append(cipherList.get(i)).append(" ");
-			sb.append("\n");
-			
-			cipherList.clear();
-			
-		}
-		
-		System.out.println(sb);
-	}
+		int x, y;
 
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			// 입력
+			n = Integer.parseInt(br.readLine().trim()); // 첫줄에 암호문 길이 입력
+			cyphers = new ArrayList<Integer>();
+
+			st = new StringTokenizer(br.readLine()); // 둘째줄에 암호문들 입력
+			for (int i = 0; i < n; i++) {
+				cyphers.add(Integer.parseInt(st.nextToken()));
+			}
+
+			m = Integer.parseInt(br.readLine()); // 셋째줄에 명령어 개수 입력
+
+			st = new StringTokenizer(br.readLine()); // 명령어들 입력.
+			for (int i = 0; i < m; i++) {
+
+				st.nextToken(); // l
+				idx = Integer.parseInt(st.nextToken()); // x
+				k = Integer.parseInt(st.nextToken()); // y
+
+				for (int j = idx; j < idx + k; j++) { // s
+					// 이거처리해줘야..
+					cyphers.add(j, Integer.parseInt(st.nextToken()));
+						
+				}
+			}
+
+			sb.append("#").append(test_case).append(" ");
+			for (int i = 0; i < 10; i++)
+				sb.append(cyphers.get(i)).append(" ");
+			sb.append("\n");
+		}
+
+		bw.append(sb);
+		bw.flush();
+		bw.close();
+		br.close();
+
+	}
 }
