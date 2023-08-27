d33c88f32ec5bb974289bfe5749c2f7dd189acc1
bum19 <77481223+bum19@users.noreply.github.com>
2023-08-27 17:05:50 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 308 ms, Memory: 42712 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,113 +1,95 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
-	public static int n, m, d, maxKill, currentKill;
-	public static int[][] board;
-	public static int[][] tmp; //탐색때 쓸 보드
-	public static int[] archers; //궁수3명 열 좌표저장
-	public static Queue<int[]> q = new ArrayDeque<int[]>(); //궁수 활범위를 bfs 탐색하기 위한 큐 [0] : y좌표, [1] : x 좌표
-	public static boolean[][] visited;
-	public static int[] dy = {0,-1,0};	 //궁수 활범위를 bfs 탐색하기 위한 방향변수, 좌,상,우
-	public static int[] dx = {-1,0,1};	 //궁수 활범위를 bfs 탐색하기 위한 방향변수, 좌,상,우
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		d = Integer.parseInt(st.nextToken());
-		
-		board = new int[n][m];
-		tmp = new int[n][m];
-		visited = new boolean[n][m];
-		archers = new int[3]; 
-		maxKill = Integer.MIN_VALUE;
-		
-		//보드 입력.
-		for(int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j =0 ; j < m; j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		//궁수배치
-		for(int i = 0; i < m; i++) {
-			for(int j = i+1; j < m; j++) {
-				for(int k = j+1; k < m; k++) {
-					//궁수자리 3곳 정하기
-					archers[0] = i;	//0번궁수 열좌표
-					archers[1] = j;	//1번궁수 열좌표
-					archers[2] = k;	//2번궁수 열좌표
-//					if(i == 1 && j == 3 && k == 5)
-//						System.out.println("20번죽이는경우의수----------------------------");
-					//초기화
-					for(int y = 0; y < n; y++) {
-						for(int x = 0 ; x< m; x++) {
-							tmp[y][x] = board[y][x];
-						}
-					}
-					currentKill = 0;
-					
-//					System.out.println("궁수자리세팅완료. 죽이기시작.");
-					//화살범위에 있는 애들 죽이기. 궁수위치 행이 n부터 1까지 총 n번 탐색. 
-					kill();
-//					if(currentKill == 20) System.out.println("i,j,k "+i+" "+j+" "+ k);
-					if(currentKill > maxKill) maxKill = currentKill;
-				}
-			}
-		}
-		System.out.println(maxKill);
-	}
-	
-	private static void kill() { 
-		////화살범위에 있는 애들 죽이기. 궁수위치 행이 n부터 1까지 총 n번 움직이며탐색.
-		for(int r = n-1; r >= 0; r--) { //r은 현재 성벽 위치 바로 위
-//			System.out.println(r+"성벽부터 탐색");
+    public static int n,m,d, maxKill;
+    public static int[] archers;
+    public static int[][] board;
+    public static int[][] tmp;
+    public static boolean[][] visited;
+    public static boolean[][] gonnaDie;
+    public static Queue<int[]> q = new ArrayDeque<>();
+    public static int[] dy = {0,-1,0}; //좌상우
+    public static int[] dx = {-1,0,1};
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-			boolean[][] gonnaDie = new boolean[n][m]; //죽을사람 위치 저장하는 리스트
-			for(int i = 0; i < 3; i++) { //왼쪽에있는 궁수부터 탐색.
-				q.clear();
-//				System.out.println(archers[i]+"번 궁수 시작");
-				for(int j = 0; j < n; j++) { //visited 초기화.
-					Arrays.fill(visited[j], false);
-				}
-				
-				q.offer(new int[] {r,archers[i],1}); //y좌표,x좌표, 현재좌표까지의 거리 입력.
-				
-				int ny, nx;
-				while(!q.isEmpty()) {
-					int[] cur = q.poll();
-//					System.out.println("("+cur[0]+", "+cur[1]+")");
-					if(tmp[cur[0]][cur[1]] == 1) { //현재 탐색한곳에 적이있으면
-//						System.out.println("적발견. 사살.");
-						gonnaDie[cur[0]][cur[1]] = true;
-						break;
-					}
-					for(int dir = 0; dir < 3; dir++) { //3방탐색
-						ny = cur[0] + dy[dir];
-						nx = cur[1] + dx[dir];
-						if(ny < 0 || nx < 0 ||ny >= n || nx >= m || cur[2] >= d || visited[ny][nx]) continue;
-						visited[ny][nx] = true; //방문처리
-						q.offer(new int[] {ny, nx, cur[2]+1});
-					}
-				}
-				
-			}
-			//죽인적 갱신
-			for(int y = 0; y < n; y++) {
-				for(int x = 0; x < m; x++) {
-					if(gonnaDie[y][x] == true) {
-						tmp[y][x] = 0;
-						currentKill++;
-					}
-				}
-			}
-			
-		}
-	}
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        maxKill = Integer.MIN_VALUE;
+        archers = new int[3];
+        board = new int[n][m];
+        tmp = new int[n][m]; //탐색에 쓸 보드
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
+        //궁수 위치 정하기 m개 열중 3곳
+        for(int i = 0; i < m; i++){
+            for(int j = i+1; j < m;j++){
+                for(int k = j+1; k < m; k++){
+                    archers[0] = i;
+                    archers[1] = j;
+                    archers[2] = k;
+//                    if(i == 0 && j == 2 && k == 4){
+//                        System.out.println("playgame start");
+//                    }
+                    playGame();
+                }
+            }
+        }
+        System.out.println(maxKill);
+    }
 
+    private static void playGame(){
+        int killCnt = 0;
+        //게임판 초기화
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                tmp[i][j] = board[i][j];
+            }
+        }
+        for(int i = n-1; i >= 0; i--){
+            gonnaDie = new boolean[n][m];//죽일사람 위치 초기화.
+            for(int j = 0; j < 3; j++) { //all archers shoot
+                visited = new boolean[n][m]; //방문초기화.
+                q.clear();
+                q.offer(new int[]{i, archers[j], 1});
+                while(!q.isEmpty()){
+                    int[] cur = q.poll();
+                    if(tmp[cur[0]][cur[1]] == 1){ //현재위치에 있으면 gonnadie 표시하고 탈출.
+                        gonnaDie[cur[0]][cur[1]] = true;
+                        break;
+                    }
+                    if(cur[2] == d) continue; //최대거리까지 왔으면 탐색종료
+                    for(int dir = 0; dir <3; dir++) {//다음위치탐색. 좌상우
+                        int ny = cur[0] + dy[dir];
+                        int nx = cur[1] + dx[dir];
+                        //인덱스범위 안이고 방문한적없으면 탐색
+                        if(ny >= 0 && nx >= 0 && ny < n && nx < m  &&!visited[ny][nx]){
+                            visited[ny][nx] = true;
+                            q.offer(new int[]{ny, nx, cur[2] + 1});
+                        }
+                    }
+                }
+            }
+
+            //제거한 적 세기
+            for(int y = 0; y < n; y++){
+                for(int x = 0; x < m; x++){
+                    if(gonnaDie[y][x]){
+                        killCnt++;
+                        tmp[y][x] = 0;
+                    }
+                }
+            }
+        }
+        //maxKill와 비교
+        if(maxKill < killCnt) maxKill = killCnt;
+    }
 }
