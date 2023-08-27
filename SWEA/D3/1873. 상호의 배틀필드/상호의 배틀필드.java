61fc5e36593037be8eb10e298e3885d5072b32ef
bum19 <77481223+bum19@users.noreply.github.com>
2023-08-27 18:04:25 +0900

    [D3] Title: 상호의 배틀필드, Time: 118 ms, Memory: 21,272 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -1,131 +1,116 @@
-//
 import java.io.*;
 import java.util.*;
 
 public class Solution {
-	public static int t, h, w, n;
-	public static char[][] map;
-	public static int[] input; //상하좌우 이동 숫자로 변환해서 저장. 슈팅명령의경우 제자리 이동으로 설정.
-	public static int[] dy = {0,-1,1,0,0}; //슛, 상하좌우 이동
-	public static int[] dx = {0,0,0,-1,1}; //슛, 상하좌우 이동
-	public static char[] tankIcon = {' ', '^','v','<','>'};
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int x = -1, y = -1, dir = -1; //초기화안해주면 컴파일 에러가 떠서 -1로 초기화 
-		t = Integer.parseInt(br.readLine().trim());
-		
-		for(int test_case = 1; test_case <= t ; test_case++) {
-			//입력
-			st = new StringTokenizer(br.readLine());
-			h = Integer.parseInt(st.nextToken());
-			w = Integer.parseInt(st.nextToken());
-			map = new char[h][w];
-			
-			for(int i = 0; i < h; i++) {
-				String line = br.readLine().trim();
-				for(int j = 0; j < w; j++) {
-					map[i][j] = line.charAt(j);
-					//현재 탱크위치 찾아서, x, y, dir값 넣기
-					if(map[i][j] ==  '^') {
-						y = i;
-						x = j;
-						dir = 1;
-					}
-					else if(map[i][j] == 'v') {
-						y = i;
-						x = j;
-						dir = 2;
-					}
-					else if(map[i][j] == '<') {
-						y = i;
-						x = j;
-						dir = 3;
-					}
-					else if(map[i][j] == '>') {
-						y = i;
-						x = j;
-						dir = 4;
-					}
-				}
-			}
-			
-			n = Integer.parseInt(br.readLine().trim());
-			input = new int[n];
-			String inputStr = br.readLine().trim();
-			
-			for(int i = 0; i < n; i++) {
-				char tmpC = inputStr.charAt(i);
-				switch(tmpC) {
-					case 'S':	input[i] = 0; break;
-					case 'U': 	input[i] = 1; break;
-					case 'D':	input[i] = 2; break;
-					case 'L':	input[i] = 3; break;
-					case 'R':	input[i] = 4; break;
-				}
-			}
-			//입력끝
-			
-			//구현
-			for(int i = 0; i < n; i++) { // 입력개수만큼 반복
-//				//테스트출력
-//				for(int a = 0; a < h; a++) {
-//					for(int b = 0; b < w; b++) {
-//						System.out.print(map[a][b]);
-//					}
-//					System.out.println();
-//				}
-//				System.out.println("--------");
-				int nx, ny;
-				
-				if(input[i] == 0) {		//입력이 0이면
-					shoot(x, y, dir);	//슈팅
-				}
-				else {					//입력이 그외면 방향전환후 이동가능하면 이동
-					dir = input[i];
-					nx = x + dx[dir];
-					ny = y + dy[dir];
-					
-					if(nx >= 0 && ny >= 0  && ny < h && nx < w && map[ny][nx] == '.') {
-						map[y][x] = '.';
-						x = nx;
-						y = ny;
-					}
-					
-					map[y][x] = tankIcon[dir];
-					
-				}
-			}
-			
-			//출력저장
-			sb.append("#").append(test_case).append(" ");
-			for(int i = 0; i < h; i++) {
-				for(int j = 0; j < w; j++) {
-					sb.append(map[i][j]);
-				}
-				sb.append("\n");
-			}
-		}
-		System.out.println(sb);
-	}
-	
-	private static void shoot(int x, int y, int dir) {
-		int nx = x + dx[dir];
-		int ny = y + dy[dir];
-		
-		while(true) {
-//			System.out.println("("+ny +", "+nx +")");
-			if(ny < 0 || nx < 0 || ny >= h || nx >= w) return; //벽밖으로 포탄 벗어나면 아무일도 일어나지 않는다.
-			if(map[ny][nx] == '#') 					   return; //강철벽 만나면 아무일도 일어나지않는다.
-			if(map[ny][nx] == '*') { 						   //벽돌벽 만날경우 부시고 종료
-				map[ny][nx] = '.';
-				return;
-			}
-			
-			nx += dx[dir];
-			ny += dy[dir];
-		}
-	}
+    public static int t,h,w,n, sx, sy;
+    public static char[][] map;
+    public static char[] commands;
+    public static int[] dy = {-1,1,0,0}; //상하좌우
+    public static int[] dx = {0,0,-1,1}; //상하좌우
+    public static char[] tankShapes = { '^', 'v', '<', '>'}; //게임판에 탱크 적을때
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        t = Integer.parseInt(br.readLine());
+        for(int test_case = 1; test_case <= t; test_case++){
+            st = new StringTokenizer(br.readLine());
+            h = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+            map = new char[h][w];
+
+            for(int i = 0; i < h; i++){ //게임판 입력
+                String str = br.readLine().trim();
+                for(int j = 0; j < w; j++){
+                    map[i][j] = str.charAt(j);
+                    if(map[i][j] == '^' || map[i][j] == 'v' || map[i][j] == '<' || map[i][j] == '>'){
+                        sy = i;
+                        sx = j;
+                    }
+                }
+            }
+
+            //명령입력
+            n = Integer.parseInt(br.readLine().trim());
+            commands = new char[n];
+            String str = br.readLine().trim();
+            for(int i = 0; i < n; i++){
+                commands[i] = str.charAt(i);
+            }
+
+            playGame();
+
+            sb.append("#").append(test_case).append(" ");
+            for(int i = 0; i < h; i++){
+                for(int j = 0; j < w; j++){
+                    sb.append(map[i][j]);
+                }
+                sb.append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    private static void playGame(){
+        //초기 위치 및 방향설정.
+        int y = sy;
+        int x = sx;
+        int dir;
+        if(map[sy][sx] == '^')      dir = 0;
+        else if(map[sy][sx] == 'v') dir = 1;
+        else if(map[sy][sx] == '<') dir = 2;
+        else                        dir = 3;
+
+        for(int i = 0; i < n; i++){
+            if(commands[i] == 'S'){ //발사
+                shoot(y,x, dir);
+            }
+            else{
+                //방향전환
+                if(commands[i] == 'U') {
+                    map[y][x] = tankShapes[0];
+                    dir = 0;
+                }
+                else if(commands[i] == 'D'){
+                    map[y][x] = tankShapes[1];
+                    dir = 1;
+                }
+                else if(commands[i] == 'L'){
+                    map[y][x] = tankShapes[2];
+                    dir = 2;
+                }
+                else if(commands[i] == 'R'){
+                    map[y][x] = tankShapes[3];
+                    dir = 3;
+                }
+
+                //이동
+                int ny = y + dy[dir];
+                int nx = x + dx[dir];
+                if(ny < 0 || nx < 0 || ny >= h || nx >= w || map[ny][nx] != '.') continue; //벗어나거나 평지아니면 이동은 하지않는다.
+                map[y][x] = '.'; //이동
+                y = ny; //이동
+                x = nx; //이동
+                map[y][x] = tankShapes[dir];
+            }
+        } //움직임 종료. 게임끝
+    }
+
+    private static void shoot(int y, int x, int dir){
+        //첫 발사 도달지 설정
+        y = y + dy[dir];
+        x = x + dx[dir];
+        while(true){
+            if(y < 0 || x < 0 || y >= h || x >= w || map[y][x] == '#') break; //인덱스벗어나거나 강철벽 만나면 종료
+
+            if(map[y][x] == '*'){ //벽돌벽만나면 부시고 종료
+                map[y][x] = '.';
+                break;
+            }
+
+            y = y + dy[dir];
+            x = x + dx[dir];
+        }
+    }
 }
