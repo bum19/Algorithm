e795adcd822793c30f51ead465cdbcbdea6da6ae
bum19 <77481223+bum19@users.noreply.github.com>
2023-10-03 10:57:42 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 126 ms, Memory: 21,440 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -1,10 +1,10 @@
 import java.io.*;
 import java.util.*;
-//Lis[i] = lis[j]+1;
+//Lis[i] = 현재까지 입력된 애들중 나보다 큰놈이있다면, 그자리에 갱신. 다 나보다 작다면, 새로 원소 추가.
 public class Solution {
     public static int t, n;
     public static int[] arr; //입력 수열
-    public static int[] dp ; //dp[i] = dp[j] + 1;  j는 arr[j] < arr[i] && j < i를 만족하는 값.
+    public static List<Integer> dp ; //dp[i] 길이 i를 만족하는 부분수열의 끝 값중 최솟값.
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,27 +16,29 @@ public class Solution {
         for(int test_case = 1; test_case <= t; test_case++){
             n = Integer.parseInt(br.readLine().trim());
             arr = new int[n];
-            dp = new int[n];
+            dp = new ArrayList<>();
             st = new StringTokenizer(br.readLine());
             for(int i = 0; i < n; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
-                dp[i] = 1; //dp 초기화
             }
 
-            //dp세팅
+            //dp 초기값 세팅
+            dp.add(arr[0]);
+
+            //부분수열 구하는 로직 시작.
             for(int i = 1; i < n; i++){
-                for(int j = i-1; j>=0; j--){
-                    if(arr[j] <= arr[i]){
-                        dp[i] = Math.max(dp[i], dp[j]+1);
+                int size = dp.size();
+                for(int j = 0; j < size; j++){
+                    if(dp.get(j) > arr[i]){ //arr[i] 보다 작은 dp값 있으면, 해당 값으로 갱신.
+                        dp.set(j, arr[i]); break;
+                    }
+                    if(j == size-1){ //현재 리스트중 arr[i]보다 작은 수가 없다면, dp에 값 추가.
+                        dp.add(arr[i]);
                     }
                 }
             }
 
-            int maxLength = Integer.MIN_VALUE;
-            for(int i = 0; i < n; i++){
-                if(maxLength < dp[i]) maxLength = dp[i];
-            }
-            sb.append("#").append(test_case).append(" ").append(maxLength).append("\n");
+            sb.append("#").append(test_case).append(" ").append(dp.size()).append("\n");
         }
         System.out.println(sb);
 
