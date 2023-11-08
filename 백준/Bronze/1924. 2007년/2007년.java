ac12963ca65e3cdcf72ed1da1b3addf14ac4c2c8
bum19 <77481223+bum19@users.noreply.github.com>
2023-11-09 01:00:23 +0900

    [Bronze I] Title: 2007년, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int mon = sc.nextInt();
-        int day = sc.nextInt();
-        int totaldays = day-1;
-        int[] daysInMonth = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-        String[] dayOfWeek = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
-
-        for(int i = 1; i < mon; i++ ){
-            totaldays += daysInMonth[i];
-        }
-
-        System.out.println(dayOfWeek[totaldays%7]);
-    }
+public class Main {
+	public static String[] yoil = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+	public static int[] days = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int mon = Integer.parseInt(st.nextToken());
+		int day = Integer.parseInt(st.nextToken());
+		
+		int totaldays = 0;
+		for(int i = 1; i <= mon-1; i++) {
+			totaldays += days[i];
+		}
+		totaldays += day-1; //1월 1일부터시작하므로
+		
+		System.out.println(yoil[totaldays%7]);
+		
+	}
 }
