7a54d31a5450d21ead05041ad0d29ee5fde31f7a
bum19 <77481223+bum19@users.noreply.github.com>
2023-11-14 01:24:46 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -1,13 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N, M, K;
-        N = sc.nextInt();
-        M = sc.nextInt();
-        K = sc.nextInt();
-        int output = Math.min(Math.min((N + M - K) / 3, N / 2), M);
-        System.out.println(output);
-    }
+	public static int n,m,k;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		int total = n + m;
+		int team;
+		if(n/2 > m) team = m;
+		else team = n/2;
+		
+		if(team > (total-k)/3) team = (total-k)/3;
+		
+		System.out.println(team);
+		
+	}
 }
