348d7eb0dd2e8de50094ebdfc695a6c939ec783f
bum19 <77481223+bum19@users.noreply.github.com>
2023-11-14 01:37:56 +0900

    [Silver IV] Title: 30, Time: 192 ms, Memory: 16056 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -1,37 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-//남의코드 복붙으로 출력초과 에러 해결되나 확인
-// 30
-// 문자열 문제
-// 시간제한 : 1초
+import java.io.*;
+import java.util.*;
 public class Main {
-	public static void main(String args[]) throws NumberFormatException, IOException {
+	public static int[] numCount;
+	public static int sum;
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String nStr = br.readLine();
-		int sum = 0;
-
-		// 해당 숫자 재배치 시작
-		char[] charArr = nStr.toCharArray();
-		Arrays.sort(charArr);	// 오름차순 정렬
-		int len = charArr.length;
-
 		StringBuilder sb = new StringBuilder();
-		// 오름차순 정렬이므로, 맨 끝 원소부터 반대로 탐색
-		for(int i = len - 1; i >= 0; i--) {
-			int num = charArr[i] - '0';
-			sum += num;
-			sb.append(num);
-			
-		}
-				
-		// 30의 배수인지 판단하기 위한 조건 생성
-		if(charArr[0] != '0' || sum % 3 != 0) {	// 만약 3의 배수가 아니라면
-			System.out.println(-1);
-			return;
+		String str = br.readLine().trim();
+		sum = 0;
+		numCount = new int[10];
+		for(int i = 0; i < str.length(); i++) {
+			numCount[str.charAt(i)-'0']++;
+			sum += str.charAt(i) - '0';
 		}
 		
-		System.out.println(sb.toString());
+		if(sum%3 == 0 && numCount[0] != 0) {
+			for(int i = 9; i >= 0; i--) {
+				for(int j = 0; j < numCount[i]; j++) {
+					sb.append(i);
+				}
+			}
+		}else {
+			sb.append(-1);
+		}
+		
+		System.out.println(sb);
 	}
 }
