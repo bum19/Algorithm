b662fad8b3e03271b275f617ae4533d67a0f8870
bum19 <77481223+bum19@users.noreply.github.com>
2023-11-30 08:44:26 +0900

    [Silver III] Title: 병든 나이트, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/1783. 병든 나이트/병든 나이트.java
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.java
@@ -1,31 +1,45 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-//6가지로나눔
-// 세로 1
-// 세로 2 , 가로<=7 / 가로 >7
-// 세로 3이상, 가로 <=4 / 4< 가로 < 7,  7<=가로
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N,M;
-        N = sc.nextInt();
-        M = sc.nextInt();
-        int answer;
+	public static int n,m, answer; //n은 세로, m은 가로.
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		//세로 1일때 1칸
+		if(n == 1) {
+			answer = 1;
+		}
+		//세로 2일때 최대 4칸. 가로 3칸 -> 2칸.가로 5칸 ->3칸, 가로 7칸 이상: 4칸
+		else if(n == 2) {
+			if( m < 7) {
+				answer = (m-1)/2 + 1;
+			}
+			else {
+				answer = 4;
+			}
+			
+		}
+		//세로3일때 최대 칸.가로 2 : 2칸, 가로3 : 3칸, 가로 4~6 : 4칸, 가로7+x : 5+x
+		else {
+			if(m < 4) {
+				answer = m;
+			}
+			else if( m < 7) {
+				answer = 4;
+			}
+			else {
+				answer = 5 + (m-7);
+			}
+		}
+		
+		System.out.println(answer);
+	}
 
-
-        if(N == 1){
-            answer = 1;
-        }
-        else if(N == 2){
-            if(M <= 7) answer = (M+1)/2;
-            else       answer = 4;
-        }
-        else{
-            if(M <=4) answer = M;
-            else if( M < 7) answer = 4;
-            else answer = 5+(M-7);
-        }
-
-        System.out.println(answer);
-    }
 }
