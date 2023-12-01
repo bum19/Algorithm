c184df06b2d63d7be481fd4d6e831e3d8e4f6e1e
bum19 <77481223+bum19@users.noreply.github.com>
2023-12-01 11:43:21 +0900

    [Gold V] Title: 강의실 배정, Time: 740 ms, Memory: 71796 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,31 +1,58 @@
-
+import java.io.*;
 import java.util.*;
 
 public class Main {
+	public static int n;
+	public static PriorityQueue<int[]> lectures; //시작순 정렬
+	public static PriorityQueue<Integer> rooms; //끝나는 시간 정렬
+	public static void main(String[] args ) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		//시작 시간 순으로
+		lectures = new PriorityQueue<int[]>(new Comparator<int[]>(){
+			public int compare(int[] a1, int[] a2) {
+				return Integer.compare(a1[0], a2[0]);
+			}
+		});
+		
+		//방은 끝나는시간 넣기. 빠른 순
+		rooms = new PriorityQueue<Integer>();
+		
+		st = new StringTokenizer(br.readLine().trim());
+		n = Integer.parseInt(st.nextToken());
+		
+		
+		for(int i = 0 ; i < n ; i++) {
+			st = new StringTokenizer(br.readLine().trim());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			lectures.add(new int[] {s,e});
+		}
+		
+		while(!lectures.isEmpty()) {
+			int[] lecture = lectures.poll();
+			//방이 있는경우
+			if(!rooms.isEmpty()) {
+				int endTime = rooms.peek();
+				//방에들어갈수있으면 
+				if(endTime <= lecture[0]) {
+					rooms.poll();
+					rooms.add(lecture[1]);
+				}
+				//없으면 추가
+				else {
+					rooms.add(lecture[1]);
+				}
+			}
+			//방이 없는경우
+			else {
+				rooms.add(lecture[1]);
+			}
+		}
+		
+		System.out.println(rooms.size());
+		
+	}
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N, tmpStart, tmpEnd, answer;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-
-        N = sc.nextInt();
-
-        // Add the classes to the priority queue
-        for (int i = 0; i < N; i++) {
-            int start = sc.nextInt();
-            int end = sc.nextInt();
-            pq.offer(new int[]{start, end});
-        }
-
-        PriorityQueue<Integer> rooms = new PriorityQueue<>();
-
-        while(!pq.isEmpty()){
-            int[] currentClass = pq.poll();
-            if(!rooms.isEmpty() && currentClass[0] >= rooms.peek()){
-                rooms.poll();
-            }
-            rooms.add(currentClass[1]);
-        }
-        System.out.println(rooms.size());
-    }
 }
