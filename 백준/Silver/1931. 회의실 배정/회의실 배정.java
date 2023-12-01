b6cb6cc11713cf55eb51d74f8a0d4a46de84686d
bum19 <77481223+bum19@users.noreply.github.com>
2023-12-01 15:10:45 +0900

    [Silver I] Title: 회의실 배정, Time: 532 ms, Memory: 41940 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,33 +1,41 @@
-import java.util.*;
-
+import java.io.*;
+import java.util.*
+;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n, start, end, currentEndTime = 0, meetingCount =0;
-        PriorityQueue<int[]> meetings = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[1] == o2[1])
-                    return o1[0] - o2[0];
-                return o1[1] - o2[1];
-            }
-        });
-
-        n = sc.nextInt();
-
-        for(int i =0; i<n; i++){
-            start = sc.nextInt();
-            end = sc.nextInt();
-            meetings.add(new int[]{start,end});
-        }
-
-        while(!meetings.isEmpty()){
-            int[] currentMeeting = meetings.poll();
-            if(currentEndTime <= currentMeeting[0]){
-                currentEndTime = currentMeeting[1];
-                meetingCount++;
-            }
-        }
-        System.out.println(meetingCount);
-    }
+	public static int n,count;
+	public static PriorityQueue<int[]> pq;
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		pq = new PriorityQueue<int[]>((a1,a2)->{
+			if(a1[1] == a2[1])
+				return Integer.compare(a1[0],a2[0]);
+			else
+				return Integer.compare(a1[1], a2[1]);
+		});
+		n = Integer.parseInt(st.nextToken());
+		
+		count = 0;
+		
+		for(int i = 0 ; i < n; i++) {
+			st = new StringTokenizer(br.readLine().trim());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			pq.add(new int[] {s,e});
+		}
+		
+		int curEndTime = 0;
+		for(int i = 0; i < n ; i++) {
+			int[] lecture = pq.poll();
+			//하나씩뽑으면서 시작시간이 현재 끝시간보다 크면 넣기
+			if(curEndTime <= lecture[0]) {
+//				System.out.println(curEndTime+" : "+lecture[0]+", "+lecture[1]);
+				count++;
+				curEndTime = lecture[1];
+			}
+		}
+		System.out.println(count);
+	}
 }
