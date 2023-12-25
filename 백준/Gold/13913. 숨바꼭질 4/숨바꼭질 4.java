22c54d5348b251c67c4474f1e9afa4d43cd522f1
bum19 <77481223+bum19@users.noreply.github.com>
2023-12-25 18:02:31 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 316 ms, Memory: 33664 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,75 +1,70 @@
 import java.io.*;
 import java.util.*;
-//시간이랑 경로 출력. 각칸에 시간이랑 경로 str로 저장하면 될듯.
+//시간이랑 경로 출력. bfs로 저장.
+//마지막 연산저장 + sb.insert로 앞에서 채우기할때 걸린시간 2696ms
+//마지막 숫자 저장 +sb.append로 채우기 할때걸린시간
 public class Main{
 	public static int n,k;
-	public static int[] dist;
-	public static char[] path; //최종적으로 어떤 연산으로 왔는지 저장.
-	public static Map<Integer, String> idxDist; // idx, dist, path
+	public static int[][] dist; //dist[0][0] : 0까지 걸린 시간, [0][1] : 0에도달하기 전 위치
+	public static boolean[] visited;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 		
-		dist = new int[100001];
-		Arrays.fill(dist,Integer.MAX_VALUE);
-		path = new char[100001];
-		
-		
-		dist[n] = 0;
-		path[n] = 0;
+		dist = new int[100001][2];
+		visited = new boolean[100001];
+//		Arrays.fill(dist,Integer.MAX_VALUE);
 		
 		Queue<int[]> q= new ArrayDeque<>();
-		q.add(new int[] {n, 0});
+		visited[n] = true;
+		q.add(new int[] {n, 0}); // curIdx, dist
 		while(!q.isEmpty()){
-			int[] idp = q.poll(); //idx, dist
-			if(idp[0] == k) break;
-			//1 plus
-			if(idp[0] + 1 <= 100000) {
-				//기존에 값 없으면 갱신 후 넣기.
-				if(dist[idp[0] + 1] == Integer.MAX_VALUE) {
-					dist[idp[0]+ 1] = idp[1] + 1;
-					path[idp[0]+ 1] = '+';
-//		System.out.println(++cnt);		
-					q.add(new int[] {idp[0]+1, idp[1]+1});
-				}
-			}
-			//1 minus
-			if(idp[0] - 1 >= 0) {
-				//기존에 값 없으면 갱신 후 넣기.
-				if(dist[idp[0] - 1] == Integer.MAX_VALUE) {
-					dist[idp[0]- 1] = idp[1] + 1;
-					path[idp[0]- 1] = '-';
-					q.add(new int[] {idp[0]-1, idp[1]+1});
-				}
-			}
-			//2 multiple
-			if(idp[0] *2 <= 100000) {
-				//기존에 값 없으면 갱신 후 넣기.
-				if(dist[idp[0]*2] == Integer.MAX_VALUE) {
-					dist[idp[0]*2] = idp[1] + 1;
-					path[idp[0]*2] = '*';
-					q.add(new int[] {idp[0]*2, idp[1]+1});
-				}
+			int[] cur = q.poll(); //idx, dist
+			int curIdx = cur[0];
+			int curDist = cur[1];
+			if(curIdx  == k) break;
+			for(int i = 0; i < 3; i++) {
+				int nextIdx = calIdx(curIdx, i);
+				if(nextIdx < 0 || nextIdx > 100000 || visited[nextIdx]) continue;
+				visited[nextIdx] = true;
+				dist[nextIdx][0] = curDist + 1;
+				dist[nextIdx][1] = curIdx;
+				q.add(new int[] {nextIdx, curDist+1});
 			}
 		}
 		
-		System.out.println(dist[k]);
-		StringBuilder sb = new StringBuilder();
+		sb.append(dist[k][0]).append("\n");
 
-		int curIdx = k;
-		while(curIdx != n) {
-			sb.insert(0, curIdx);
-			sb.insert(0, ' ');
-			if(path[curIdx] == '+')		curIdx--;
-			else if(path[curIdx] == '-')curIdx++;
-			else if(path[curIdx] == '*')curIdx /= 2;
-		}
-		sb.insert(0, curIdx);
-		System.out.println(sb);
+		recurAdd(k, sb);
 		
+		System.out.println(sb.toString().trim()); //맨끝 공백 제거
+		
+	}
+	
+	public static int calIdx(int idx, int num) {
+		if(num == 0)
+			return idx +1;
+		else if(num == 1)
+			return idx -1;
+		else if(num == 2)
+			return idx *2;
+		else
+			return -1;
+	}
+	
+	public static void recurAdd(int curIdx, StringBuilder sb) {
+		if(curIdx == n) {
+			sb.append(curIdx).append(" ");
+			return;
+		}
+		
+		recurAdd(dist[curIdx][1], sb);
+		
+		sb.append(curIdx).append(" ");
 	}
 }
