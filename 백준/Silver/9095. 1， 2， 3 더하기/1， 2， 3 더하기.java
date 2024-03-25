1b181d79afaf932cd36ed1ae3f2fcde86c0f2a10
bum19 <77481223+bum19@users.noreply.github.com>
2024-03-25 21:36:49 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,35 +1,28 @@
-import java.util.Scanner;
-
+import java.io.*;
+import java.util.*;
+/*
+ * dp[i] = i를 만드는데 가능한 방법
+ * dp[i] = dp[i-1]에서 1원추가 + dp[i-2]에서 2원추가 + d[i-3]에서 3원추가.
+ */
 public class Main {
-    public static int cnt;
-    public static int T,n;
-    public static int[] arr= new int[4];
-    public static void sum(int num){
-        if(num >= n){
-            if(num == n){
-                cnt++;
-            }
-            return;
-        }
-
-        for(int i = 1;i<=3;i++){
-            sum(num+i);
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        T = sc.nextInt();
-        for (int i = 0; i < T; i++) {
-            n = sc.nextInt();
-            cnt = 0;
-            sum(0);
-            sb.append(cnt);
-            if (i != T - 1)
-                sb.append("\n");
-        }
-
-        System.out.println(sb);
-    }
+	public static int t, n;
+	public static int[] dp;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		t = Integer.parseInt(br.readLine().trim());
+		
+		for(int test_case = 1; test_case <= t; test_case++) {
+			n = Integer.parseInt(br.readLine().trim());
+			dp = new int[n+1];
+			dp[0] = 1;
+			dp[1] = 1;
+			if(n >= 2) dp[2] = 2;
+			for(int i = 3; i <= n; i++) {
+				dp[i] = dp[i-1]+ dp[i-2] + dp[i-3];
+			}
+			sb.append(dp[n]).append("\n");
+		}
+		System.out.println(sb);
+	}
 }
