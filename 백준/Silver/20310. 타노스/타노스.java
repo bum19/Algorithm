ce59561d96cb3b737604c51999855934af7e8100
bum19 <77481223+bum19@users.noreply.github.com>
2024-06-09 13:57:37 +0900

    [Silver III] Title: 타노스, Time: 132 ms, Memory: 14236 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -1,5 +1,5 @@
 /*
- * 인덱스를 들고, 0인 인덱스와 1인 인덱스를 원소로하는 리스트 두개를 병합하는 방식으로 코딩
+ * 1은뒤에서, 0은 앞에서 세고, 살릴 인덱스만 true 처리
  */
 import java.io.*;
 import java.util.*;
@@ -7,14 +7,13 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		List<Integer> zeroIndice = new ArrayList<>();
-		List<Integer> oneIndice = new ArrayList<>();
+
+		
 		
 		int zeroCnt = 0;
 		int oneCnt = 0;
 		
 		String input = br.readLine().trim();
-		
 		//0개수, 1개수 세기
 		for(int i = 0; i < input.length(); i++) {
 			if(input.charAt(i) == '0') zeroCnt++;
@@ -22,10 +21,11 @@ public class Main {
 		}
 		
 		//0 반만큼 인덱스 세기. 앞에서 센다.
+		boolean[] isAlive = new boolean[input.length()];
 		int cnt = zeroCnt;
 		for(int i = 0; i< input.length(); i++) {
 			if(input.charAt(i) == '0') {
-				zeroIndice.add(i);
+				isAlive[i] = true;
 				cnt--;
 			}
 			
@@ -36,39 +36,21 @@ public class Main {
 		cnt = oneCnt;
 		for(int i = input.length()-1; i >= 0; i--) {
 			if(input.charAt(i) == '1') {
-				oneIndice.add(i);
+				isAlive[i] = true;
 				cnt--;
 			}
 			if(cnt == oneCnt/2) break;
 		}
-		//1 리스트 거꾸로해주기
-		oneIndice.sort(Collections.reverseOrder());
 		
 		//새로운 input 만들기
 		StringBuilder sb = new StringBuilder();
-		int zeroIdx = 0;
-		int oneIdx = 0;
-		while(true) {
-			if(zeroIdx >= zeroIndice.size() || oneIdx >= oneIndice.size()) {
-				break;
-			}
-			if(zeroIndice.get(zeroIdx) < oneIndice.get(oneIdx)) {
-				sb.append(0);
-				zeroIdx++;
-			}
-			else {
-				sb.append(1);
-				oneIdx++;
+		for(int i = 0; i < input.length(); i++) {
+			if(isAlive[i]) {
+				sb.append(input.charAt(i));
 			}
 		}
 		
-		//둘중 남은것 채워주기
-		for(; zeroIdx < zeroIndice.size(); zeroIdx++) {
-			sb.append(0);
-		}
-		for(; oneIdx < oneIndice.size(); oneIdx++) {
-			sb.append(1);
-		}
+
 		
 		//출력
 		System.out.println(sb);
