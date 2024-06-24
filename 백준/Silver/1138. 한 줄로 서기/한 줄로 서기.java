1128c9e4dd77f2c967df068c73ae58ae954567c0
bum19 <77481223+bum19@users.noreply.github.com>
2024-06-25 01:36:05 +0900

    [Silver II] Title: 한 줄로 서기, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,74 +1,35 @@
 /*
- * n이 10이므로, 멍청하게 순열떄려도 10! * 10^2(확인시간)이므로 충분히 시간내에 확인가능
+ * 답보고 다시 품.
+ * 큰애부터 줄 세우기. 왼쪽에 큰애만큼의 인덱스위치로 넣으면 된다. 
+ * 한번삽입시 최대 1+2+3+4+...+10회 이동이 일어날 수 있으므로, 최대 시간복잡도 55.
  */
 import java.io.*;
 import java.util.*;
 public class Main {
-	public static int n;
-	public static int[] arr;
-	public static int[] input;
-	public static boolean[] visited;
-	public static boolean isAnswerFound;
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n];
-		input = new int[n];
-		visited = new boolean[n];
+		int n = Integer.parseInt(br.readLine());
+		int[] input = new int[n];
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n ; i++) {
 			input[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		isAnswerFound = false;
-		perm(0);
-	}
 	
-	
-	private static void perm(int depth) {
-		if(isAnswerFound) return;
-		
-		if(depth == n) {
-			check();
-			return;
+		List<Integer> answer = new ArrayList<>();
+		answer.add(n);
+		for(int i = n-2; i >= 0; i--) {
+			answer.add(input[i], i+1);
 		}
 		
-		for(int i = 0; i < n; i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				arr[depth] = i;
-				perm(depth+1);
-				visited[i] = false;
-			}
-		}
-	}
-	
-	private static void check() {
-		for(int i = 0 ; i< n; i++) {
-			int curH = arr[i];
-			//현재 키가, 왼쪽에 몇명이나 있나 확인.
-			int tallCnt = 0;
-			for(int j = 0; j < i; j++) {
-				if(arr[j] > curH)
-				tallCnt++;
-			}
-			if(tallCnt != input[curH]) {
-				return;
-			}
-		}
-		
+		//print
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < n; i++) {
-			if(i != n-1) {
-				sb.append(arr[i]+1).append(' ');
-			}
-			else
-				sb.append(arr[i]+1);
+		for(int i = 0; i < n-1; i++) {
+			sb.append(answer.get(i)).append(' ');
 		}
-		
+		sb.append(answer.get(n-1));
 		System.out.println(sb);
-		isAnswerFound = true;
 	}
 }
