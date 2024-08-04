c316ba69f6eb3562ae8e6a42aed3f4f0f820f7ff
bum19 <77481223+bum19@users.noreply.github.com>
2024-08-04 15:35:21 +0900

    [Gold III] Title: 불!, Time: 460 ms, Memory: 52624 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -2,82 +2,74 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int r,c, escapeTime;
-	public static char[][] map;
-	public static int[] jStart;
-	public static List<int[]>  fStart;
-	public static boolean[][] isJVisited;
-	public static boolean[][] isFVisited;
-	public static int[] dy = {-1,1,0,0}; //상하좌우
-	public static int[] dx = {0,0,-1,1}; //상하좌우
-	public static void main(String[] args) throws IOException{
+	
+	public static char[][] maze;
+	public static int r,c;
+	
+	public static int[] dy = {-1,0,1,0}; //상우하좌
+	public static int[] dx = {0,1,0,-1};
+	
+	public static void main(String[] args )throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		
-		String[] input;
+		st = new StringTokenizer(br.readLine());
 		
-		input = br.readLine().trim().split(" ");
-		r = Integer.parseInt(input[0]);
-		c = Integer.parseInt(input[1]);
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
 		
-		map = new char[r][c];
-		isJVisited = new boolean[r][c];
-		isFVisited = new boolean[r][c];
-		fStart = new ArrayList<int[]>();
+		maze = new char[r][c];
+		List<int[]> baseFire = new ArrayList<>();
+		int[] jStart = null;
 		
-		for(int i = 0 ; i < r; i++) {
+		for(int i = 0; i < r; i++) {
 			String tmp = br.readLine().trim();
-			for(int j = 0; j < c; j++) {
-				map[i][j] = tmp.charAt(j);
-				if(map[i][j] == 'J') {
-					jStart = new int[] {i,j};
-				}
-				else if(map[i][j] == 'F') {
-					fStart.add(new int[] {i,j});
-				}
+			for(int j = 0; j < c ; j++){
+				maze[i][j] = tmp.charAt(j);
+				if(maze[i][j] == 'F') baseFire.add(new int[]{i,j});
+				if(maze[i][j] == 'J') jStart = new int[]{i,j};
 			}
 		}
 		
-		//input done
-		escapeTime = -1; //init
-		Queue<int[]> q = new ArrayDeque<>();
-		isJVisited[jStart[0]][jStart[1]] = true;
-		q.offer(new int[] {'j', jStart[0],jStart[1], 0});
-		for( int[] f : fStart) {
-			isFVisited[f[0]][f[1]] = true;
-			q.offer(new int[] {'f', f[0],f[1]});
+		int time = bfs(jStart,baseFire);
+		if(time == -1) {
+			System.out.println("IMPOSSIBLE");
 		}
+		else {
+			System.out.println(time);
+		}
+	}
+	
+	private static int bfs(int[] jStart, List<int[]> baseFire) {
+		Queue<int[]> q = new ArrayDeque<>(); //y,x, time
+		
+		q.add(new int[] {jStart[0],jStart[1],0});
+		for(int[] fire : baseFire) {
+			q.add(new int[] {fire[0],fire[1], 0});
+		}
+		
 		while(!q.isEmpty()) {
-			int cur[] = q.poll();
-			//j일경우, 현재위치가 불 안 붙어있을 경우만 진행
-			if(cur[0] == 'j' && !isFVisited[cur[1]][cur[2]]) {
-				//현재 가장자리라면 게임끝
-				if(cur[1] == 0 || cur[1] == r-1 || cur[2] == 0 || cur[2] == c-1) {
-					escapeTime = cur[3] + 1;
-					break;
+			int[] cur = q.poll();
+			for(int dir = 0; dir <4; dir++) {
+				int ny = cur[0] + dy[dir];
+				int nx = cur[1] + dx[dir];
+				if(maze[cur[0]][cur[1]] == 'J') {
+					if(ny < 0 || nx < 0 || ny>=r || nx >= c) {
+						return cur[2] + 1;
+					}
+					if(maze[ny][nx] == '.') {
+						maze[ny][nx] = 'J';
+						q.add(new int[] {ny,nx,cur[2]+1});
+					}
 				}
-				for(int dir = 0; dir < 4; dir++) {
-					int ny = cur[1] + dy[dir];
-					int nx = cur[2] + dx[dir];
-					//가장자리체크할필요는 없다
-					if(map[ny][nx] == '#' || isFVisited[ny][nx] || isJVisited[ny][nx]) continue;
-					isJVisited[ny][nx] = true;
-					q.add(new int[] {'j',ny,nx, cur[3]+1});
-				}
-			}
-			//f일 경우.
-			else if(cur[0] == 'f'){
-				for(int dir = 0; dir < 4; dir++) {
-					int ny = cur[1] + dy[dir];
-					int nx = cur[2] + dx[dir];
-					if(ny < 0 || nx < 0 || ny >= r || nx >= c || map[ny][nx] == '#' || isFVisited[ny][nx]) continue;
-					isFVisited[ny][nx] = true;
-					q.add(new int[] {'f',ny,nx});
+				else {
+					if(ny < 0 || nx < 0 || ny >= r || nx >= c || maze[ny][nx] == '#' || maze[ny][nx] == 'F') continue;
+					maze[ny][nx] = 'F';
+					q.add(new int[] {ny,nx,cur[2]+1});
 				}
 			}
-		}//end while
+		}
 		
-		if(escapeTime == -1) System.out.println("IMPOSSIBLE");
-		else				 System.out.println(escapeTime);
+		return -1;
 	}
 }
