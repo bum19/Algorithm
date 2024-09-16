9a555452d31d50758fcdc15741cf0f943fb470ee
bum19 <77481223+bum19@users.noreply.github.com>
2024-09-16 16:26:27 +0900

    [Silver II] Title: 창고 다각형, Time: 108 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,52 +1,72 @@
 /*
- * 예전에 내가푼방식: 제일 긴거부터 탐색
- * 이번에 풀 방식 : 진용방식
- * 시간복잡도 : O(n * maxH)
+ * 그리디한 풀이.
+ * 정렬후 가장 큰 막대기를 찾은뒤, 왼쪽 오른쪽 진행하는 방식
  */
 import java.io.*;
 import java.util.*;
-
 public class Main {
-	public static int[][] board;
-	public static int[][] input;
-	public static int n, maxH;
-	
+
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		n = Integer.parseInt(br.readLine());
-		input = new int[n][2];
 		
-		maxH = -1;
-		for(int i = 0; i < n; i++) {
+		int n = Integer.parseInt(br.readLine());
+		
+		Stick[] sticks = new Stick[n];
+		for(int i = 0; i< n; i++) {
 			st = new StringTokenizer(br.readLine());
-			input[i][0] = Integer.parseInt(st.nextToken());
-			input[i][1] = Integer.parseInt(st.nextToken());
-			maxH = Math.max(maxH, input[i][1]);
+			int l = Integer.parseInt(st.nextToken());
+			int h = Integer.parseInt(st.nextToken());
+			sticks[i] = new Stick(l,h);
 		}
 		
-		Arrays.sort(input, (a1, a2)->(Integer.compare(a1[0],a2[0])));
+		Arrays.sort(sticks, (s1, s2)-> (Integer.compare(s1.l, s2.l)));
 		
-		int answer =0;
-		for(int i = 1; i <= maxH; i++) {
-			int left = 0;
-			int right = n-1;
-			while(left < n) {
-				if(input[left][1] >= i) {
-					break;
-				}
-				left++;
-			}
-			while(right >= 0) {
-				if(input[right][1] >= i) {
-					break;
-				}
-				right--;
-			}
-			if(left <= right) {
-				answer += input[right][0]-input[left][0]+1;
+		//find maxH
+		int maxIdx = 0;
+		int maxH = -1;
+		for(int i  = 0; i < n; i++) {
+			if(sticks[i].h > maxH) {
+				maxH = sticks[i].h;
+				maxIdx = i;
 			}
 		}
+		
+		int answer = 0;
+		
+		int preL = -1;
+		int preH = 0;
+		//left ~ maxH
+		for(int i = 0; i <= maxIdx; i++) {
+			if(sticks[i].h >= preH) {
+				answer += (sticks[i].l - preL) * preH;
+				preL = sticks[i].l;
+				preH = sticks[i].h;
+			}
+		}
+		
+		preL = -1;
+		preH = 0;
+		//right ~ maxH
+		for(int i = n-1; i>= maxIdx; i--) {
+			if(sticks[i].h >= preH) {
+				answer += (preL - sticks[i].l) * preH;
+				preL = sticks[i].l;
+				preH = sticks[i].h;
+			}
+		}
+		
+		answer += preH;
+		
 		System.out.println(answer);
+		
+	}
+	
+	public static class Stick{
+		int l,h;
+		public Stick(int l, int h) {
+			this.l = l;
+			this.h = h;
+		}
 	}
 }
