8eeb7bafa90211ec729b49011634907a17e3dbb9
bum19 <77481223+bum19@users.noreply.github.com>
2024-10-07 19:37:49 +0900

    [Gold V] Title: 용액, Time: 324 ms, Memory: 28736 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,81 +1,45 @@
+/*
+ * 풀이 1. 이분탐색으로 0에 가까워지게 반복하기
+ */
 import java.io.*;
 import java.util.*;
-//이분탐색힌트봄.
 public class Main {
-	public static List<Integer> san;
-	public static List<Integer> yum;
-	public static int answer1, answer2, closestNum, n;
+	public static int n;
+	public static int[] input;
+	
 	public static void main(String[] args) throws IOException{
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
 		n = Integer.parseInt(br.readLine().trim());
+		input = new int[n];
 		
-		san = new ArrayList<Integer>(); 
-		yum = new ArrayList<Integer>();
-		
-		//입력 오름차순으로 주어짐. 음수는 내림차순, 양수는 오름차순으로 입력받기
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
-			int tmp = Integer.parseInt(st.nextToken());
-			if(tmp < 0) {
-				san.add(tmp);
+		for(int i = 0; i < n ;i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int l = 0;
+		int r = n-1;
+		
+		int min = Integer.MAX_VALUE;
+		int minL= 0, minR =0;
+		while(l < r) {
+			int cur = input[l] + input[r];
+			if(Math.abs(cur) < Math.abs(min)) {
+				min = cur;
+				minL = input[l];
+				minR = input[r];
+			}
+			if(cur < 0) l++;
+			else if(cur == 0) {
+				System.out.println(input[l]+" "+input[r]);
+				return;
 			}
 			else {
-				yum.add(tmp);
+				r--;
 			}
 		}
 		
-		closestNum = Integer.MAX_VALUE;
-		
-		for(int i = 0 ; i < san.size(); i++) {
-			int cur = san.get(i);
-			int s = 0;
-			int e = yum.size()-1;
-			
-			while(s <= e) {
-				int tmp = cur + yum.get((s+e)/2);
-//				System.out.println(cur+", "+yum.get((s+e)/2));
-				if(closestNum > Math.abs(tmp)) {
-					answer1= cur;
-					answer2= yum.get((s+e)/2);
-					closestNum = Math.abs(tmp);
-				}
-				if(tmp < 0) s = (s+e)/2 + 1;
-				else if(tmp > 0) e = (s+e)/2 - 1;
-				else break;
-			}
-			
-		}
-		
-		//산성만 2개 더하기
-		if(san.size() >= 2) {
-			int tmp1 = san.get(san.size()-2);
-			int tmp2 = san.get(san.size()-1);
-			
-			if(closestNum > Math.abs(tmp1+tmp2)) {
-				closestNum = Math.abs(tmp1+tmp2);
-				answer1 = tmp1;
-				answer2 = tmp2;
-			}
-			
-		}
-		
-		//알칼리성만 2개 더하기
-		if(yum.size() >= 2) {
-			int tmp1 = yum.get(0);
-			int tmp2 = yum.get(1);
-			
-			if(closestNum > Math.abs(tmp1+tmp2)) {
-				closestNum = Math.abs(tmp1+tmp2);
-				answer1 = tmp1;
-				answer2 = tmp2;
-			}
-			
-		}
-		
-		System.out.println(answer1+" "+answer2);
-		
+		System.out.println(minL +" "+ minR);
 	}
 }
